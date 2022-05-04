008245e31e83be6f0014846cad189b078901e3b8
krishna23-addweb <krishna.a.addweb@gmail.com>
2022-05-04 10:21:21 +0530

    drupalize and ERP

---
+++ ERP/sites/default/files/php/twig/626fd4b937600___string_template__308792_mqEqwWVjZxZB5dvSBOSW8flrn/TfaqxJqPEraEJh5XgmRT1GJhUz5KEhTcJiWRbCa54mI.php
@@ -0,0 +1,84 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__308792e460ba75b0f1efe1bc41260ca814c9b5aaa2bb91fe9df9b4900c46e592 */
+class __TwigTemplate_13a398dcce9b4071fadc4ee44c0544db4e8d69a16970b83570375bc5fbf6cad7 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "destination=/admin/structure/views/view/students/preview/page_1%3F_wrapper_format%3Ddrupal_ajax";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__308792e460ba75b0f1efe1bc41260ca814c9b5aaa2bb91fe9df9b4900c46e592";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__308792e460ba75b0f1efe1bc41260ca814c9b5aaa2bb91fe9df9b4900c46e592", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ ERP/sites/default/files/php/twig/626fd4b937600___string_template__868c91_lDCu-tQ8RVtjU85_LDZdq5WVN/5Qxz6oeWAt3xBZ1rXLXL3YND428z335cVAh8_DIxUVg.php
@@ -0,0 +1,84 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__868c91a3c2a7437530410d33b7950cd313e38fc573ef9af7528fb24b20387852 */
+class __TwigTemplate_e5bfb4ebd912c8e6b97d9807eb79d5480d47809d9337869e439fa7079dcc7343 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "destination=/admin/structure/views/view/students/preview/block_2%3F_wrapper_format%3Ddrupal_ajax";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__868c91a3c2a7437530410d33b7950cd313e38fc573ef9af7528fb24b20387852";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__868c91a3c2a7437530410d33b7950cd313e38fc573ef9af7528fb24b20387852", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
